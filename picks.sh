cf67a2208899639c59dc1c07f8add8982d4e9b5a
Francescodario Cuzzocrea <bosconovic@gmail.com>
2023-03-19 22:28:38 +0100

    update
    Change-Id: I8a215bfd1743b0d1761750a02d7a3fba14251c7b

--- picks.sh
+++ picks.sh
@@ -21,17 +21,26 @@ repopick 345753 # teegris: Label /dev/tziwshmem
 # repopick 330087 # sepolicy: add super fast charge interface # To be pushed to 20
 
 # device/samsung_slsi/sepolicy
+repopick 351271 # common: Add policy for hwc3
 # repopick 330092 # sepolicy: add policy for super fast charge # To be pushed to 20
 
 # -------------- PLATFORM STUFF --------------
 
 # build
-repopick 334342 # Sorry bro: 6 -> 3
+repopick -f 334342 # Sorry bro: 6 -> 3
 
 # build/soong
 # repopick 320142 # Add dumpvars for WITH_EXYNOS_BSP # To be pushed to 20
 
 # frameworks/base
+repopick 347946 # SystemUI: Network traffic with center cutout [1/3]
+repopick 350954 # SystemUI: Make edit button click ripple effect rounded
+repopick 350953 # SystemUI: DreamTile: Enable for everyone
+repopick 350952 # SystemUI: Follow light/dark theme in SplitShade Header
+repopick 350951 # SystemUI: Show the alarm icon on QS as well
+repopick 350286 # SystemUI: Bring in drawable for statusbar firewall toggle
+repopick 350285 # Implement firewall status bar indicator
+
 repopick 340916 # SystemUI: add burnIn protection
 repopick -f 334343 # Alter model name to avoid SafetyNet HW attestation enforcement
 repopick -f 334344 # keystore: Block key attestation for SafetyNet
@@ -44,8 +53,10 @@ repopick -f 334344 # keystore: Block key attestation for SafetyNet
 # repopick 330096 # hidl: fastcharge: add 1.1 impl # To be pushed to 20
 
 # lineage-sdk
+repopick 347915 # Network traffic with center cutout [2/3]
 
 # packages/apps/LineageParts
+repopick 347916 # Network traffic with center cutout [3/3]
 
 # packages/apps/Settings
 # repopick 330083 # Settings: add super fast charge toggle # To be pushed to 20
@@ -56,7 +67,9 @@ repopick -f 334344 # keystore: Block key attestation for SafetyNet
 repopick -f 334348 # init: Set properties to make SafetyNet pass
 
 # vendor/lineage
+repopick 351331 # overlay: Disable immersive mode confirmation
 repopick -f 334345 # lineage: Disable privapp permission enforcement (make it log)
 repopick -f 334346 # adb insecure by default
 
+repopick -t charging-control-hal-20
 exit 0
