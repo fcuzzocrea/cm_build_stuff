b7fe0b32bc002cbb77276e9978c7f6da3df9e1a4
Francescodario Cuzzocrea <bosconovic@gmail.com>
2023-05-08 08:51:22 +0200

    update
    Change-Id: I74c2d3f91e4298b2474cf9af24fe664a7df47480

--- picks.sh
+++ picks.sh
@@ -26,15 +26,7 @@ repopick 351271 # common: Add policy for hwc3
 repopick -f 334342 # Sorry bro: 6 -> 3
 
 # frameworks/base
-repopick 350952 # SystemUI: Follow light/dark theme in SplitShade Header
-repopick 351877 # Remove wtf if no display nits mapping exists
-repopick 351878 # Adapt tuner prefs to S settings style & cleanup
-repopick 351893 # fixup! Keyguard: Add option to scramble pin layout when unlocking (2/2).
-repopick 351855 # core: Set config_displayLightSensorType to android.sensor.light
-repopick 351858 # JobScheduler: Respect allow-in-power-save perm
-repopick 351262 # Camera: Expose aux camera if packagename is null
-repopick 349283 # PhoneWindowManager: Fix wakeup and long-press actions of assistant key
-# repopick 340916 # SystemUI: add burnIn protection
+repopick 340916 # SystemUI: add burnIn protection
 repopick -f 334343 # Alter model name to avoid SafetyNet HW attestation enforcement
 repopick -f 334344 # keystore: Block key attestation for SafetyNet
 
@@ -46,10 +38,6 @@ repopick 351016 # aidl: vibrator: Only handle actually supported effects
 repopick 351017 # aidl: vibrator: Add click and tick duration config properties
 # repopick 330095 # fastcharge: move 1.0 impl to separate folder # To be pushed to 20
 # repopick 330096 # hidl: fastcharge: add 1.1 impl # To be pushed to 20
-
-# lineage-sdk
-
-# packages/apps/LineageParts
 
 # packages/apps/Settings
 # repopick 330083 # Settings: add super fast charge toggle # To be pushed to 20
