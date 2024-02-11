ee5049d887f78b30132ad29212adb4fa0cc13c28
Francescodario Cuzzocrea <bosconovic@gmail.com>
2024-02-11 21:29:38 +0100

    refresh picks

--- picks.sh
+++ picks.sh
@@ -44,6 +44,7 @@ repopick -f -P frameworks/base 357510 # gmscompat: also spoof props for samsung/
 # hardware/samsung_slsi-linaro/configs
 repopick 382352 # configs: rename 990 to 9830
 repopick 382353 # exynos9830: update variables for T bsp
+repopick 382436 # BoardConfig9830: Switch to Vulkan UI renderer
 
 # hardware/samsung_slsi-linaro/exynos
 repopick 381617 # kernel-*headers: Symlink videodev2 headers to linux/
@@ -51,13 +52,12 @@ repopick 381618 # kernel-4.4-headers: videodev2_exynos_media: Add NV12M definiti
 repopick 381619 # libhwjpeg: Add backwards compatibility with older camera blobs
 
 # hardware/samsung_slsi-linaro/graphics
-repopick 381622 # graphics: Add exynos7885(/7904) configuration
 repopick 381623 # libhwjpeg: Add backwards compatibility with older camera blobs
-repopick 381624 # graphics: base: Import lib[g]scaler from Android 10 BSP sources
 repopick 382354 # exynos9830: update brightness paths
 repopick 382355 # exynos9830: switch to EXYNOS_SET_DISPLAY_MODE
 repopick 382356 # fixup! graphics: Support EXYNOS_SET_DISPLAY_MODE ioctl
 repopick 382357 # fixup! hwc3: Update HalImpl for T BSP
+repopick 382435 # base: hwcomposer_property: move properties to system_ext
 
 # hardware/samsung_slsi-linaro/interfaces
 repopick 382406 # interfaces: do not build libGeoTrans for 9830
