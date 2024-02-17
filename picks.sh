ba25c2e81f85fd9ca78146bbb83ec9525dfb07d2
Francescodario Cuzzocrea <bosconovic@gmail.com>
2024-02-17 18:03:29 +0100

    refresh picks

--- picks.sh
+++ picks.sh
@@ -29,6 +29,7 @@ repopick -f 369796 # Sorry bro: 6 -> 3
 
 # frameworks/base
 # repopick 340916 # SystemUI: add burnIn protection
+repopick -f -P frameworks/base 383057 #wm: Optimize window/transition animation scaling
 repopick -f -P frameworks/base 369786 # Alter model name to avoid SafetyNet HW attestation enforcement
 repopick -f -P frameworks/base 369787 # keystore: Block key attestation for SafetyNet
 repopick -f -P frameworks/base 369788 # AttestationHooks: Set shipping level to 32 for devices >=33
@@ -45,7 +46,7 @@ repopick 380288 # doze: bump target SDK version
 # hardware/samsung_slsi-linaro/configs
 repopick 382352 # configs: rename 990 to 9830
 repopick 382353 # exynos9830: update variables for T bsp
-# repopick 382436 # BoardConfig9830: Switch to Vulkan UI renderer
+repopick 382436 # BoardConfig9830: Switch to Vulkan UI renderer
 
 # hardware/samsung_slsi-linaro/exynos
 repopick 381617 # kernel-*headers: Symlink videodev2 headers to linux/
@@ -72,9 +73,6 @@ repopick 382406 # interfaces: do not build libGeoTrans for 9830
 
 # packages/apps/Glimps
 repopick 381899 # Glimpse: Drop trash confirmation dialogs
-
-# packages/apps/Seedvault
-repopick -p 382137 # Merge branch 'android14' of https://github.com/seedvault-app/seedvault
 
 # packages/apps/Settings
 repopick 381429 # Handle mobile data on internet page during setup
